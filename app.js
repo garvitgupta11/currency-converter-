0ec16218b289fe1f029ba53efcb8c561e6d0f99e
Garvit Gupta <garvitgupta921917@gmail.com>
2026-07-29 23:21:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+const BASE_URL = "https://latest.currency-api.pages.dev/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+    for (let currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = true;
+        } else if (select.name === "to" && currCode === "INR") {
+            newOption.selected = true;
+        }
+
+        select.append(newOption);
+    }
+
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    try {
+        const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+
+        const response = await fetch(URL);
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch exchange rate");
+        }
+
+        const data = await response.json();
+
+        const rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+
+        const finalAmount = (amtVal * rate).toFixed(2);
+
+        msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+    } catch (error) {
+        console.error(error);
+        msg.innerText = "Unable to fetch exchange rate.";
+    }
+};
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let img = element.parentElement.querySelector("img");
+    img.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
+};
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+    updateExchangeRate();
+});
